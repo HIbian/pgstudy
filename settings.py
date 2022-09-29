168034b6529bc465f0f2d8f6fcd5fb82defaf9c3
hibian <416711267@qq.com>
2022-09-29 20:54:37 +0800

    init repository

---
+++ settings.py
@@ -0,0 +1,11 @@
+from pygame.math import Vector2
+# game information
+GAME_NAME = 'hibian\'s practice'
+GAME_VERSION = '0.0.1'
+
+# screen
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+TILE_SIZE = 64
+
+
