d5bc1ebd635705d4f1342d7ce90c4f631da4d260
hibian <416711267@qq.com>
2022-10-02 00:08:54 +0800

    overlay - ui

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 from pygame.math import Vector2
+
 # game information
 GAME_NAME = 'hibian\'s practice'
 GAME_VERSION = '0.0.1'
@@ -8,4 +9,8 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 TILE_SIZE = 64
 
-
+# overlay positions
+OVERLAY_POSITIONS = {
+    'tool': (40, SCREEN_HEIGHT - 15),
+    'seed': (100, SCREEN_HEIGHT - 15)
+}
