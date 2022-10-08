d54eddec623908d3c39c4bf8bb795095dfb9d129
hibian <416711267@qq.com>
2022-10-09 00:15:13 +0800

    respawn position

--- level.py
+++ level.py
@@ -52,7 +52,10 @@ class Level:
             groups=self.all_sprites,
             z=LAYERS['ground']
         )
-        self.player = Player((640, 360), self.all_sprites,self.collision_sprites)
+        # Player
+        for obj in tmx_data.get_layer_by_name('Player'):
+            if obj.name == 'Start':
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
 
     def run(self, dt):
         self.display_surface.fill('black')
